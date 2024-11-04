92a31986c2d5ea7b236834f29f70a5980e5e6ee8
Diamond Francis <hello@diamondfrancis.co.uk>
2024-11-04 18:52:04 +0000

    test

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
+/** code by webdevtrick ( https://webdevtrick.com ) **/
 (function($) { 
   $(function() { 
-    // Toggle dropdown menu on click
     $('nav ul li a:not(:only-child)').click(function(e) {
-      const dropdown = $(this).siblings('.nav-dropdown');
-      dropdown.toggle();
-      dropdown.attr('aria-expanded', dropdown.is(':visible'));
-      $('.nav-dropdown').not(dropdown).hide().attr('aria-expanded', 'false');
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.dropdown').not($(this).siblings()).hide();
       e.stopPropagation();
     });
-
-    // Hide dropdown when clicking outside
     $('html').click(function() {
-      $('.nav-dropdown').hide().attr('aria-expanded', 'false');
+      $('.nav-dropdown').hide();
     });
-
-    // Toggle mobile navigation
     $('#nav-toggle').click(function() {
       $('nav ul').slideToggle();
+    });
+    $('#nav-toggle').on('click', function() {
       this.classList.toggle('active');
     });
   }); 
